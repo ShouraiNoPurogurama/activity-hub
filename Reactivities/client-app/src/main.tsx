7d947cadadd7ec112aca6e68c9f58bf1b68baa1e
NA <nhatanhtruong20@gmail.com>
2024-08-28 00:04:05 +0700

    FE: Refactor homepage styles, install react calendar

--- Reactivities/client-app/src/main.tsx
+++ Reactivities/client-app/src/main.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app/layout/App.tsx'
+import 'react-calendar/dist/Calendar.css'
 import 'semantic-ui-css/semantic.min.css'
 import './app/layout/styles.css'
 import { store, StoreContext } from './app/stores/store.ts'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './app/router/Route.tsx'
-
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
